ce8d43395b5470ba0c737a118428e694e48c47bf
thaihs0002 <79821996+thaihs0002@users.noreply.github.com>
2026-01-13 22:03:51 +0700

    Refactor Otto movements and improve servo control

--- main/boards/otto-robot/otto_movements.cc
+++ main/boards/otto-robot/otto_movements.cc
@@ -1,14 +1,8 @@
 #include "otto_movements.h"
+#include <cmath>
 #include <algorithm>
-#include "freertos/idf_additions.h"
-#include "oscillator.h"
 
 static const char* TAG = "OttoMovements";
-
-// Cấu hình biên độ dao động (Độ rộng góc quay)
-#define AMP_HEAD_PAN   20  // Đầu quay trái phải 20 độ
-#define AMP_HEAD_TILT  15  // Đầu gật 15 độ
-#define AMP_HANDS      30  // Tay vung 30 độ
 
 Otto::Otto() {
     is_otto_resting_ = false;
@@ -28,24 +22,25 @@ unsigned long IRAM_ATTR millis() {
 }
 
 void Otto::Init(int left_leg, int right_leg, int left_foot, int right_foot, int left_hand, int right_hand) {
-    servo_pins_[LEFT_LEG] = left_leg;     // Dùng cho ĐẦU QUAY (Yaw)
-    servo_pins_[RIGHT_LEG] = right_leg;   // Dùng cho ĐẦU GẬT (Pitch)
-    servo_pins_[LEFT_FOOT] = left_foot;   // (Không dùng)
-    servo_pins_[RIGHT_FOOT] = right_foot; // (Không dùng)
-    servo_pins_[LEFT_HAND] = left_hand;   // TAY TRÁI
-    servo_pins_[RIGHT_HAND] = right_hand; // TAY PHẢI
+    servo_pins_[LEFT_LEG] = left_leg;
+    servo_pins_[RIGHT_LEG] = right_leg;
+    servo_pins_[LEFT_FOOT] = left_foot;
+    servo_pins_[RIGHT_FOOT] = right_foot;
+    servo_pins_[LEFT_HAND] = left_hand;
+    servo_pins_[RIGHT_HAND] = right_hand;
 
     has_hands_ = (left_hand != -1 && right_hand != -1);
     AttachServos();
-    is_otto_resting_ = false;
+    Home();
 }
 
 void Otto::AttachServos() {
     for (int i = 0; i < SERVO_COUNT; i++) {
         if (servo_pins_[i] != -1) {
             oscillators_[i].Attach(servo_pins_[i]);
-            // Cài đặt tốc độ giới hạn để không bị giật
-            oscillators_[i].EnableLimiter(50); 
+            oscillators_[i].SetTrim(servo_trim_[i]);
+            // Giới hạn tốc độ để mượt hơn
+            oscillators_[i].EnableLimiter(60); 
         }
     }
 }
@@ -67,57 +62,54 @@ void Otto::SetTrims(int left_leg, int right_leg, int left_foot, int right_foot, 
     servo_trim_[RIGHT_HAND] = right_hand;
     
     for (int i = 0; i < SERVO_COUNT; i++) {
-        oscillators_[i].SetTrim(servo_trim_[i]);
+        if (servo_pins_[i] != -1) {
+            oscillators_[i].SetTrim(servo_trim_[i]);
+        }
     }
 }
 
 void Otto::Home() {
-    if (is_otto_resting_) return;
-
     for (int i = 0; i < SERVO_COUNT; i++) {
-        oscillators_[i].SetPosition(90); // Về giữa (90 độ)
-        oscillators_[i].Stop();          // Dừng dao động
+        if (servo_pins_[i] != -1) {
+            oscillators_[i].Stop();
+            oscillators_[i].SetPosition(90);
+        }
     }
     is_otto_resting_ = true;
 }
 
-// --- CHỨC NĂNG MỚI: CHUYỂN ĐỘNG TRÌNH BÀY MƯỢT MÀ ---
 void Otto::StartSpeakingMode() {
     is_otto_resting_ = false;
     
-    // 1. Đầu quay trái phải (Yaw) - Chậm rãi
-    oscillators_[LEFT_LEG].SetO(0);         // Offset
-    oscillators_[LEFT_LEG].SetA(AMP_HEAD_PAN); 
-    oscillators_[LEFT_LEG].SetT(3000);      // Chu kỳ 3 giây (Rất mượt)
-    oscillators_[LEFT_LEG].SetPh(0);
+    // 1. Đầu quay trái phải (Yaw)
+    oscillators_[LEFT_LEG].SetO(0);
+    oscillators_[LEFT_LEG].SetA(20); // Biên độ 20 độ
+    oscillators_[LEFT_LEG].SetT(3000); // Chu kỳ 3s mượt mà
     oscillators_[LEFT_LEG].Play();
 
     // 2. Đầu gật nhẹ (Pitch)
     oscillators_[RIGHT_LEG].SetO(0);
-    oscillators_[RIGHT_LEG].SetA(AMP_HEAD_TILT);
-    oscillators_[RIGHT_LEG].SetT(2000);     // Chu kỳ 2 giây
-    oscillators_[RIGHT_LEG].SetPh(M_PI/2);  // Lệch pha để không trùng nhịp quay
+    oscillators_[RIGHT_LEG].SetA(15);
+    oscillators_[RIGHT_LEG].SetT(2000);
+    oscillators_[RIGHT_LEG].SetPh(M_PI/2); // Lệch pha
     oscillators_[RIGHT_LEG].Play();
 
-    // 3. Tay trái vung nhẹ
+    // 3. Hai tay trình bày
     if (has_hands_) {
-        oscillators_[LEFT_HAND].SetO(30);   // Nâng tay lên một chút
-        oscillators_[LEFT_HAND].SetA(AMP_HANDS);
+        oscillators_[LEFT_HAND].SetO(30); 
+        oscillators_[LEFT_HAND].SetA(30);
         oscillators_[LEFT_HAND].SetT(2500);
-        oscillators_[LEFT_HAND].SetPh(0);
         oscillators_[LEFT_HAND].Play();
 
-        // 4. Tay phải vung nhẹ (ngược chiều tay trái)
         oscillators_[RIGHT_HAND].SetO(30);
-        oscillators_[RIGHT_HAND].SetA(AMP_HANDS);
+        oscillators_[RIGHT_HAND].SetA(30);
         oscillators_[RIGHT_HAND].SetT(2500);
-        oscillators_[RIGHT_HAND].SetPh(M_PI); // Ngược pha
+        oscillators_[RIGHT_HAND].SetPh(M_PI); // Ngược pha tay trái
         oscillators_[RIGHT_HAND].Play();
     }
 }
 
 void Otto::UpdateSpeakingMotion() {
-    // Hàm này phải được gọi liên tục trong vòng lặp để cập nhật vị trí Servo theo hình sin
     for (int i = 0; i < SERVO_COUNT; i++) {
         if (servo_pins_[i] != -1) {
             oscillators_[i].Refresh();
@@ -125,14 +117,11 @@ void Otto::UpdateSpeakingMotion() {
     }
 }
 
-// Các hàm cũ giữ nguyên (Move, Walk...) nếu cần, nhưng ta tập trung vào SpeakingMode
 void Otto::_moveServos(int time, int servo_target[]) {
-    // Giữ nguyên code cũ để tương thích
-    AttachServos();
-    if (GetRestState()) SetRestState(false);
-    
-    // Logic di chuyển thẳng servo (ít dùng trong mode Speaking này)
-    // ... (Code cũ)
+    // Hàm này giữ lại để tránh lỗi Linker nếu code khác có gọi
+    for (int i = 0; i < SERVO_COUNT; i++) {
+        if (servo_pins_[i] != -1) {
+            oscillators_[i].SetPosition(servo_target[i]);
+        }
+    }
 }
-
-// ... Giữ lại các hàm Walk/Jump cũ nếu muốn ...
